946db7ebf5414e436237e33e592117b2c477d477
William Hoffman <whoffman1989@gmail.com>
2019-09-27 21:59:00 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/* 
+GAME FUNCTION:
+    - Player must guess a number between a min and max
+    - Player gets a certain amount of guesses
+    - Notify player of guesses remaining
+    - Notify the player of the correct answer if the lose
+    - Let the player choose to play again
+*/
