35edeffc4c63eb1543c71f2873d5530233e30bdc
William Hoffman <whoffman1989@gmail.com>
2019-09-27 22:20:49 -0600

    added initial JS for winning number and styling

--- app.js
+++ app.js
@@ -6,3 +6,48 @@ GAME FUNCTION:
     - Notify the player of the correct answer if the lose
     - Let the player choose to play again
 */
+
+// Game Values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for players guess
+guessBtn.addEventListener("click", function() {
+  let guess = parseInt(guessInput.value);
+
+  // Validate guess
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  // Check if guess = winning number
+  if (guess === winningNum) {
+    // Disable Input
+    guessInput.disabled = true;
+    // Change Border Color
+    guessInput.style.borderColor = "green";
+    // Set Message
+    setMessage(`${winningNum} is correct, YOU WIN!`, "green");
+  } else {
+  }
+});
+
+// Set Message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
